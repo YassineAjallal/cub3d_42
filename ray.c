0e06051c8e9d2530696c5d6027c436350704fecd
yassine542 <101680065+yassine542@users.noreply.github.com>
2023-06-03 13:17:43 +0100

    map validation

--- ray.c
+++ ray.c
@@ -15,12 +15,54 @@ void *ptr;
 void *win;
 void *img;
 
+int ft_s(int s0, int s1)
+{
+	if (s0 < s1)
+		return (1);
+	return (-1);
+}
+
+void my_line(int x0, int y0, int x1, int y1, void *mlx, void *win)
+{
+	int dx;
+	int dy; 
+	int err;
+	int e2;
+
+	dx = abs(x1 - x0);
+	dy = abs(y1 - y0);
+	if (dx > dy)
+		err = dx / 2;
+	else
+		err = -dy / 2;
+	while (1)
+	{
+		mlx_pixel_put(mlx, win,x0, y0, 0xffff);
+		
+
+		if ( x0== x1 && y0 == y1) 
+			break;
+		e2 = err;
+		if (e2 > -dx) 
+		{ 
+			err -= dy;
+			x0 += ft_s(x0, x1); 
+		}
+		if (e2 < dy) 
+		{
+			err += dx;
+			y0 += ft_s(y0, y1); 
+		}
+	}
+
+}
+
 void	draw_look_at(void *img, int line_len, int bpp, int x, int y, float pa, int view)
 {
 	float px = x + cos(pa) * view;
 	float py = y + sin(pa) * view;
 	int index = line_len * (int)py + (int)px * (bpp / 8);
-	img_pix_put((t_img *)img, (int)px, (int)py, 0x00FF0000);
+	// img_pix_put((t_img *)img, (int)px, (int)py, 0x00FF0000);
 }
 
 int	key_hook(int keycode, void *param)
@@ -77,8 +119,9 @@ int main(int argc, char const *argv[])
 	img = mlx_xpm_file_to_image(ptr, "./Shoot.xpm", &width, &width);
 	pdx = cos(pa) * 5;
 	pdy = sin(pa) * 5;
-	draw_look_at(img, 1280 * 4, 32, x, y, pa, view);
+	// draw_look_at(img, 1280 * 4, 32, x, y, pa, view);
 	mlx_put_image_to_window(ptr, win, img, x, y);
+	my_line(x, y, pdx, pdy, ptr, win);
 	mlx_hook(win, 2, 0, key_hook, NULL);
 	mlx_loop(ptr);
 	return 0;
